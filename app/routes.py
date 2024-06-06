4bbf2ae3e18c22f83844b35339a219e609677adc
Jean Bougeois-Cruz <jrb14b@gmail.com>
2024-06-06 11:55:18 -0400

    Update layout

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
 from flask import render_template
-from . import create_app
-
-app = create_app()
+from flask import current_app as app
 
 @app.route('/')
 def home():
+    print("Home route accessed")
     return render_template('index.html', title='Home')
 
 @app.route('/resume/')
